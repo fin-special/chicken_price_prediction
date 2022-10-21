94b0e1624d551d3b6631f754b28f50bb9002e1fe
JungKi Kim <73014464+omago123@users.noreply.github.com>
2022-10-21 11:06:41 +0900

    브랜치 테스트

--- 크롤링/네이버뉴스.py
+++ 크롤링/네이버뉴스.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
+#branch test 
 
 def getUrl(startdate,enddate,urlList,filename):
     
